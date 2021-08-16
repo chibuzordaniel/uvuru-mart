1f0ebf7e4702f9735b46c2134790866d56aaa94c
chibuzordaniel <starchrischibuzor@gmail.com>
2021-08-17 10:19:11 +1300

    update file

--- src/src/index.js/main.js
+++ src/src/index.js/main.js
@@ -1,1 +1,19 @@
-// alert('hello chibuzor')
+let menuOpen = document.querySelector('.menu-open');
+let menuClose = document.querySelector('.menu-close');
+let dropDown = document.querySelector('.drop-down');
+
+menuOpen.addEventListener('click', clickOpen);
+menuClose.addEventListener('click', clickClose)
+
+
+function clickOpen () {
+    menuOpen.style.display="none"
+    menuClose.style.display="block"
+    dropDown.style.display="block"
+};
+
+function clickClose () {
+    menuOpen.style.display="block"
+    menuClose.style.display="none"
+    dropDown.style.display="none"
+}
