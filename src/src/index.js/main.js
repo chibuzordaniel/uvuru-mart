10ce2f9552a7075cf6447382f961603cba09b1e2
chibuzordaniel <starchrischibuzor@gmail.com>
2021-09-25 10:22:52 +1300

    update file

--- src/src/index.js/main.js
+++ src/src/index.js/main.js
@@ -17,3 +17,58 @@ function clickClose () {
     menuClose.style.display="none"
     dropDown.style.display="none"
 }
+
+
+// const contentitem = document.querySelector('.content-item')
+// framecontentitem = document.querySelector('.item')
+
+// loadEvenListeners();
+
+// function loadEvenListeners(){
+//     contentitem.addEventListener('click', items)
+// }
+
+
+// function items(e) {
+//     e.preventDefault();
+//     if(e.target.classList.contains('mapo')){
+//         const contentitem = e.target.parentElement.parentElement;
+
+//         getcontentitemInfo(contentitem)
+//     }
+// }
+
+
+// function getcontentitemInfo(contentitem) {
+
+//        const contentitemInfo = {
+//            item: contentitem.querySelector('.item svg'),
+//            masu: contentitem.querySelector('.masu'),
+//            masu: contentitem.querySelector('.masu '),
+//            masu: contentitem.querySelector('.masu span svg'),
+//            mapo:  contentitem.querySelector('.mapo'),
+//        }
+//        console.log(contentitemInfo)
+// }
+
+// function mapo(contentitem) {
+
+//     const content = document.createElement('item');
+
+//     content.innerHTML = `
+//     <div class="item">
+//             <h1>${contentitem.root}<h1/>
+//             <p>${contentitem.masu}<p/>
+//             <p>${contentitem.root}<p>
+//             <p>${contentitem.note}<p>
+            
+         
+//        <div/>
+    
+//     `;
+//     framecontentitem.appendChild(content)
+// }
+
+
+
+function
